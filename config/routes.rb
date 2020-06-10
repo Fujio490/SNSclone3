08400e41f4bc2b5bbba59995711d05b45cadefdf
Naoya Fujio <naofudiwo@gmail.com>
2020-06-10 08:24:51 +0000

    g sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get '/', to: 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
