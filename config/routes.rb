65ce54e998c1695ac07cf2e72893dd182c781087
Naoya Fujio <naofudiwo@gmail.com>
2020-06-10 01:43:27 +0000

    apply routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :favorites, only: [:create, :destroy]
 end
