803083a2264afdf772286dcd3f352bb86877964c
Naoya Fujio <naofudiwo@gmail.com>
2020-06-17 05:53:47 +0000

    apply show_favorites_function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,4 +7,7 @@ class FavoritesController < ApplicationController
       favorite = current_user.favorites.find_by(id: params[:id]).destroy
       redirect_to blogs_url, notice: "#{favorite.blog.user.name}さんのブログをお気に入り解除しました"
     end
+    def index
+      @favorites = Favorite.all
+    end
   end
