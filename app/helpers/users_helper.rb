5ea6738c686b5133d322bd01f02a8575f169a3f4
Naoya Fujio <naofudiwo@gmail.com>
2020-06-18 05:26:16 +0000

    fix post_function

--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-module UsersHelper
-    def choose_new_or_edit
-        if action_name == 'new' || action_name == 'create'
-            users_path
-        elsif action_name == 'edit'
-            user_path
-        end
-    end
-end
